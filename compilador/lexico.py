2b7ee8b31deaee71ea89667d197aa9426713c50a
Carlos Eduardo X. Bezerra <47821159+Carlos-Xavier@users.noreply.github.com>
2022-11-20 18:47:45 -0300

    correção de bugs / adicionado linhas e colunas

--- compilador/lexico.py
+++ compilador/lexico.py
@@ -1,55 +1,76 @@
 from token import Token
+from symbols import Symbols
 
 class Lexico:
+    symbols_lex = Symbols()
+    
     def __init__(self, filename) -> None:
-        self.__content = self.readFile(filename)
+        self.__content = ''
+        self.__fileLines = self.readFile(filename)
         self.__state = 0
         self.__position = 0
-        self.__nextpostion = 0
+        self.__column = 0
+        self.__line = 0
 
     def readFile(self, filename) -> list:
-        temp = []
+        lines = []
         try:
             with open(filename) as f:
-                temp = [character for line in f.readlines() for character in line]
+                lines = [line for line in f.readlines()]
             f.close()
         except:
             print('Error')
 
-        return temp
+        return lines
 
+    def readLines(self):
+        for index, line in enumerate(self.__fileLines):
+            self.__line = index
+            self.__content = line
+
+            if line[0] == ' ':
+                self.startWithSpace()
+
+            self.getToken()
+
+            self.__position = 0
+            self.__column = 0
+
+    def startWithSpace(self):
+        count = 0
+        for ignore in self.__content:
+            if ignore != ' ': break
+            count += 1
+
+        self.__column += count
+                
     def getToken(self) -> Token:
-        current = self.__content[self.__position]
-        while True:
-            if not self.__isEOF():
-                current = self.__content[self.__position]
-
-            if self.__isEOF() and self.__state == 0: 
-                break
-
+        while self.__position < len(self.__content):
+            current = self.__content[self.__position]
             self.__match(current)
             self.__next()
             
 
     def __match(self, current: str) -> Token:
+        # print("opa", current, self.__position)
         match self.__state:
             case 0:
-                if self.__isChar(current):
+                if self.symbols_lex.isChar(current):
                     self.__state = 1
-                elif self.__isNumber(current):
+                elif self.symbols_lex.isNumber(current):
                     self.__state = 2
-                elif self.__isOperator(current):
+                elif self.symbols_lex.isOperator(current):
                     self.__state = 3
-                elif self.__other(current):
+                elif self.symbols_lex.other(current):
                     self.__state = 4
             case 1:
-                if not self.__isChar(current) and not self.__isNumber(current):
+                if not self.symbols_lex.isChar(current) and not self.symbols_lex.isNumber(current):
                     self.__createToken(Token.TK_IDENTIFIER)
             case 2:
-                if not self.__isChar(current) and not self.__isNumber(current):
+                if not self.symbols_lex.isChar(current) and not self.symbols_lex.isNumber(current):
                     self.__createToken(Token.TK_NUMBER)
             case 3:
-                if not self.__isOperator(current):
+                if not self.symbols_lex.isOperator(current):
                     self.__createToken(Token.TK_OPERATOR)
             case 4:
                 self.__createToken(Token.TK_OTHER)
@@ -58,37 +79,30 @@ class Lexico:
     def __createToken(self, type):
         token = Token()
         token.setType(type)
-        token.setText(''.join(self.__content[self.__nextpostion:self.__position]).strip())
+        token.setLexema(''.join(self.__content[self.__column:self.__position]).strip())
+        token.setLine(self.__line+1)
+        token.setColumn(self.__column+1)
 
-        self.__nextpostion = self.__position
+        print(token)
+
+
+        if not self.symbols_lex.ignore(self.__content[self.__position]):
+            self.__column = self.__position
+        else:
+            self.__column = self.__position + 1
+
         self.__back()
         self.__state = 0
-        print(token)
 
     def __next(self) -> str:
         self.__position += 1
 
     def __isEOF(self) -> bool:
         return self.__position == len(self.__content)
-
-    def __isChar(self, c: str) -> bool:
-        return (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z')
-
-    def __isNumber(self, c: str) -> bool:
-        return c >= '0' and c <= '9'
-
-    def __isOperator(self, c: str) -> bool:
-        return c == '>' or c == '<' or c == '=' or c == '!'
-
-    def __ignore(self, c: str) -> bool:
-        return c == '\t' or c == '\n' or c == ' ' or c == '\r'
-    
-    def __other(self, c: str) -> bool:
-        return c == ';' or c =='(' or c == ')' or c == '{' or c == '}'
     
     def __back(self) -> None:
         self.__position -= 1
 
 
 test = Lexico("file.txt")
-test.getToken()
+test.readLines()
--- compilador/token.py
+++ compilador/token.py
@@ -4,24 +4,58 @@ class Token:
     TK_IDENTIFIER  = 0
     TK_NUMBER      = 1
     TK_OPERATOR    = 2
-    TK_OTHER   = 3
+    TK_OTHER       = 3
     TK_ASSIGN      = 4
-    __type = None
-    __text = None
+
+    namesType = {
+        0: 'TK_IDENTIFIER',
+        1: 'TK_NUMBER',
+        2: 'TK_OPERATOR',
+        3: 'TK_OTHER',
+        4: 'TK_ASSIGN'
+    }
+
+    def __init__(self) -> None:
+        self.__type = None
+        self.__nameType = None
+        self.__lexema = None
+        self.__line = None
+        self.__column = None
+
 
     def __str__(self) -> str:
-        return f'Token [Type: {self.__type}, Text: {self.__text}]'
+        
+        return '%s[%04d, %04d] (%04d, %20s) (%s)' % (' '*14,self.__line,self.__column,self.__type, self.__nameType, self.__lexema)
 
     def getType(self) -> int:
         return self.__type
 
     def setType(self, type) -> None:
         self.__type = type
+        self.__nameType = self.namesType[type]
 
-    def getText(self) -> str:
-        return self.__text
+    def getNameType(self) -> str:
+        return self.__nameType
 
-    def setText(self, text) -> None:
-        self.__text = text
+    def setNameType(self, text) -> None:
+        self.__nameType = text
+
+    def getLexema(self) -> str:
+        return self.__lexema
+
+    def setLexema(self, text) -> None:
+        self.__lexema = text
+
+    def getLine(self) -> str:
+        return self.__line
+
+    def setLine(self, text) -> None:
+        self.__line = text
+
+    def getColumn(self) -> str:
+        return self.__column
+
+    def setColumn(self, text) -> None:
+        self.__column = text
 
     
