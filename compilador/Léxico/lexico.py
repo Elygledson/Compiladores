1ca49f466869b88510e9b2655b1b0eb179edbeb9
Carlos-Xavier <cexb@ic.ufal.br>
2023-03-28 11:11:11 -0300

    Update Léxico

---
+++ compilador/Léxico/lexico.py
@@ -0,0 +1,208 @@
+import pickle
+from symbols import Symbols
+import os
+import sys
+from token import Token
+
+
+# Incompleto
+Symbols_table = {}
+
+
+class Lexico:
+    symbols_lex = Symbols()
+
+    def __init__(self, filename) -> None:
+        self.__content = ''
+        self.__fileLines = self.readFile(filename)
+        self.__state = 0
+        self.__currentTokenPosition = 0
+        self.__column = 0
+        self.__line = 0
+        self.__tokens = []
+
+    def readFile(self, filename) -> None:
+        lines = []
+        try:
+            with open(os.path.join(os.path.dirname(sys.argv[0]), filename)) as f:
+                lines = [line for line in f.readlines()]
+            f.close()
+        except Exception as e:
+            print('Error', e)
+
+        return lines
+
+    def getTokens(self) -> list:
+        return self.__tokens
+
+    def readLines(self):
+        for line, content in enumerate(self.__fileLines):
+            self.__line = line
+            self.__content = content
+
+            self.__column = 0
+            self.__currentTokenPosition = 0
+            while self.__column < len(self.__content):
+                self.__match(self.__content[self.__column])
+
+                if self.symbols_lex.ignore(self.__content[self.__column]):
+                    self.__currentTokenPosition += 1
+
+                self.__column += 1
+
+        if self.__state != 0:
+            self.createToken(self.__state)
+
+    def __match(self, current: str) -> None:
+
+        match self.__state:
+            case 0:
+                if self.symbols_lex.isChar(current):
+                    self.__state = 1
+                elif self.symbols_lex.isNumber(current):
+                    self.__state = 2
+                elif self.symbols_lex.isOperator(current):
+                    self.__state = 3
+                elif self.symbols_lex.other(current):
+                    self.__state = 4
+                elif current == ':':
+                    self.__state = 7
+                elif current == "#":
+                    self.__state = 8
+                elif self.symbols_lex.isRelacional(current):
+                    self.__state = 9
+                else:
+                    if not self.symbols_lex.ignore(current):
+                        print('Error', current, self.__line, self.__column)
+
+            case 1:
+                if self.symbols_lex.isChar(current) or self.symbols_lex.isNumber(current):
+                    self.__state = 1
+                else:
+                    self.createToken(1)
+                    if not self.symbols_lex.ignore(current):
+                        self.back()
+
+            case 2:
+                if self.symbols_lex.isNumber(current):
+                    self.__state = 2
+                elif current == ',':
+                    self.__state = 6
+                else:
+                    self.createToken(2)
+                    if not self.symbols_lex.ignore(current):
+                        self.back()
+
+            case 3:
+                if self.symbols_lex.isOperator(current):
+                    self.__state = 3
+                else:
+                    self.createToken(3)
+                    if not self.symbols_lex.ignore(current):
+                        self.back()
+
+            case 4:
+                self.createToken(4)
+                if not self.symbols_lex.ignore(current):
+                    self.back()
+
+            case 6:
+                if self.symbols_lex.isNumber(current):
+                    self.__state = 6
+                else:
+                    self.createToken(6)
+                    if not self.symbols_lex.ignore(current):
+                        self.back()
+
+            case 7:
+                if current == '=':
+                    self.__column += 1
+                    self.createToken(8)
+                    if not self.symbols_lex.ignore(current):
+                        self.back()
+                else:
+                    self.createToken(7)
+                    self.back()
+
+            case 8:
+                if current == "#":
+                    self.__state = 8
+                else:
+                    self.createToken(9)
+                    if not self.symbols_lex.ignore(current):
+                        self.back()
+
+            case 9:
+                if self.symbols_lex.isRelacional(current):
+                    self.__state = 9
+                else:
+                    self.createToken(10)
+                    if not self.symbols_lex.ignore(current):
+                        self.back()
+
+    def createToken(self, type):
+        token = self.__content[self.__currentTokenPosition: self.__column].strip(
+        )
+
+        if type == 1:
+            if token in Token.reservedWord:
+                self.__tokens.append(
+                    {'value': token, 'token': Token.reservedWord[token]})
+            else:
+                if token not in Symbols_table:
+                    position = len(Symbols_table)
+                    Symbols_table[token] = {'position': position}
+                else:
+                    position = Symbols_table[token]['position']
+                self.__tokens.append(
+                    {'value': token, 'token': Token.TK_IDENTIFIER, 'position': position})
+
+        elif type == 2:
+            self.__tokens.append(
+                {'value': token, 'token': Token.TK_NUMBER})
+
+        elif type == 3:
+            self.__tokens.append(
+                {'value': token, 'token': Token.operador[token]})
+
+        elif type == 4:
+            self.__tokens.append(
+                {'value': token, 'token': Token.others[token]})
+
+        elif type == 6:
+            self.__tokens.append(
+                {'value': token, 'token': Token.TK_FLOAT})
+
+        elif type == 7:
+            self.__tokens.append(
+                {'value': token, 'token': Token.TK_TWO_POINTS})
+
+        elif type == 8:
+            self.__tokens.append(
+                {'value': token, 'token': Token.TK_ASSIGN})
+
+        elif type == 9:
+            self.__tokens.append(
+                {'value': token, 'token': Token.TK_COMMENT})
+
+        elif type == 10:
+            if token in Token.relacional:
+                self.__tokens.append(
+                    {'value': token, 'token': Token.relacional[token]})
+
+        self.__currentTokenPosition = self.__column
+        self.__state = 0
+
+    def back(self):
+        self.__column -= 1
+
+
+lexical = Lexico("file.txt")
+lexical.readLines()
+
+# with open("save.pickle", "wb") as f:
+#     pickle.dump(lexical.getTokens(), f)
+print(lexical.getTokens())
+
+
+print(Symbols_table)
---
+++ compilador/Léxico/token.py
@@ -0,0 +1,97 @@
+
+
+class Token:
+    TK_IDENTIFIER = 'TK_IDENTIFICADOR'
+    TK_NUMBER = 'TK_NUMERO'
+    TK_OPERATOR = 'TK_OPERADOR'
+    TK_OTHER = 'TK_OTHER'
+    TK_ASSIGN = 'TK_ATRIBUICAO'
+    TK_FLOAT = 'TK_FLOAT'
+    TK_ERROR = 'TK_ERRO'
+    TK_STRING = 'TK_STRING'
+    TK_CHAR = 'TK_CARACTERE'
+    TK_RELATIONAL = 'TK_RELACIONAL'
+    TK_TWO_POINTS = 'TK_DOIS_PONTOS'
+    TK_COMMENT = 'TK_COMENTARIO'
+
+    reservedWord = {
+        'program': "RW_PROGRAMA",
+        'begin': "RW_BEGIN",
+        "end": "RW_END",
+        "var": "RW_VAR",
+        "integer": "RW_INTEGER",
+        "real": "RW_REAL",
+        "of": "RW_OF",
+        "pilha": "RW_PILHA",
+        "procedure": "RW_PROCEDURE",
+        "function": "RW_FUNCAO",
+        "read": "RW_READ",
+        "write": "RW_WRITE",
+        "for": "RW_FOR",
+        "to": "RW_TO",
+        "do": "RW_DO",
+        "repeat": "RW_REPEAT",
+        "until": "RW_UNTIL",
+        "while": "RW_WHILE",
+        "if": "RW_IF",
+        "then": "RW_THEN",
+        "else": "RW_ELSE",
+        "concatena": "RW_CONCATENA",
+        "inverte": "RW_INVERT",
+        "input": "RW_INPUT",
+        "output": "RW_OUTPUT",
+        "length": "RW_LENGTH",
+    }
+
+    others = {
+        "(": "TK_ABRIR_PARENTESES",
+        ")": "TK_FECHAR_PARENTESES",
+        ",": "TK_VIRGULA",
+        ";": "TK_PONTO_VIRGULA",
+        "[": "TK_ABRIR_COLCHETES",
+        "]": "TK_FECHAR_COLCHETES",
+        ".": "TK_PONTO",
+        "\"": "TK_ASPAS_DUPLAS",
+        "\'": "TK_ASPAS_SIMPLES"
+    }
+
+    operador = {
+        "+": "TK_ADICAO",
+        "-": "TK_SUBTRACAO",
+        "*": "MULTIPLICACAO",
+        "/": "TK_DIVISAO",
+        "//": "TK_DIVISAO_INTEIRA"
+    }
+
+    relacional = {
+        ">": "TK_MAIOR",
+        "<": "TK_MENOR",
+        ">=": "TK_MAIOR_IGUAL",
+        "<=": "TK_MENOR_IGUAL",
+        "==": "TK_IGUAL"
+    }
+
+    def __init__(self, nameType, lexeme, line, column) -> None:
+        self.__nameType = nameType
+        self.__type = self.tokenCategory[nameType]
+        self.__lexema = lexeme
+        self.__line = line
+        self.__column = column
+
+    def __str__(self) -> str:
+        return '%s[%04d, %04d] (%04d, %20s) (%s)' % (' '*14, self.__line, self.__column, self.__type, self.__nameType, self.__lexema)
+
+    def getType(self) -> int:
+        return self.__type
+
+    def getNameType(self) -> str:
+        return self.__nameType
+
+    def getLexema(self) -> str:
+        return self.__lexema
+
+    def getLine(self) -> str:
+        return self.__line
+
+    def getColumn(self) -> str:
+        return self.__column
